662587cd9152eadbc4925f70d5c76d146431fa3a
Mike <mike.levy.rva@gmail.com>
2022-06-25 13:45:57 -0400

    added html ids to hour row divs and created trackhours func in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,32 @@ $("#currentDay").text(currentDate)
 
 
 
+function trackHours() {
+    var currentHour = moment().hour(); 
+
+    $(".time-block").each(function () {
+        var hourRow = parseInt($(this).attr("id").split("hour")[1]);
+        // console.log( hourRow, currentHour)
+
+        if (hourRow < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (hourRow === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+trackHours(); 
+
 
 
 
