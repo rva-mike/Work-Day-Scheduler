08d5531b5e0b165a52905ba1e0e0f91ceb56670c
Mike <mike.levy.rva@gmail.com>
2022-06-25 14:04:47 -0400

    added save and load functionality w/ click listener and localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
+
 //display current date using moment.js and jquery 
 var currentDate = moment().format("dddd, MMMM Do")
 $("#currentDay").text(currentDate)
 
 
-
-
-
 function trackHours() {
-    var currentHour = moment().hour(); 
+    var currentHour = moment().hour();
 
     $(".time-block").each(function () {
         var hourRow = parseInt($(this).attr("id").split("hour")[1]);
@@ -30,27 +28,31 @@ function trackHours() {
         }
     })
 }
-trackHours(); 
+trackHours();
+
+
+//click listener for save button that gets user input and hour row data
+$(".saveBtn").on("click", function () {
+    // console.log(this);
+    var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+    var hour = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+
+    //setting localStorage
+    localStorage.setItem(hour, text);
+})
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//get/load data from localStorage
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
 
 
